50ed6600f3dc12c050ea03ea7df70728a31b1787
Rohit Deka <studyrhd@gmail.com>
2025-04-18 21:45:48 +0530

    coonectDb Fixed

--- src/index.ts
+++ src/index.ts
@@ -6,71 +6,71 @@ import process from 'process';
 import cluster from 'cluster';
 
 
-const numCPUs = availableParallelism();
+// const numCPUs = availableParallelism();
 const PORT = process.env.PORT || 8000;
 
 
 
-if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
-    for (let i = 0; i < 4; i++) {
-      cluster.fork();
-    }
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-    });
-}
-else{
-    connectDB()
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(` Worker ${process.pid} running on PORT: ${PORT}`);
-      });
-    })
-    .catch((err) => {
-      console.error(" Error running server", err);
-    });
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// connectDB()
-// .then(()=>{
-//     app.listen(PORT, () => {
+// if (cluster.isPrimary) {
+//     console.log(`Primary ${process.pid} is running`);
+//     for (let i = 0; i < 4; i++) {
+//       cluster.fork();
+//     }
+//     cluster.on('exit', (worker, code, signal) => {
+//       console.log(`worker ${worker.process.pid} died`);
+//     });
+// }
+// else{
+//     connectDB()
+//     .then(() => {
+//       app.listen(PORT, () => {
 //         console.log(` Worker ${process.pid} running on PORT: ${PORT}`);
 //       });
 //     })
-// .catch((err)=>{
-//     console.log("Error running server ",err)
-// })
+//     .catch((err) => {
+//       console.error(" Error running server", err);
+//     });
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(PORT, () => {
+        console.log(` Worker ${process.pid} running on PORT: ${PORT}`);
+      });
+    })
+.catch((err)=>{
+    console.log("Error running server ",err)
+})
 
