08611b6e4d8ad29d375fe990965869daf094e34b
Rohit Deka <studyrhd@gmail.com>
2025-04-18 21:14:31 +0530

    types-fix

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -13,6 +13,19 @@ import { Request } from "express";
 import { IUser } from "../../models/user.model";
 export interface IRequest extends Request{
   user?: IUser;
-  
+  user?:Document;
+  user?: {
+    _id: string;
+    fullname?: string;
+    username?: string;
+    email?: string;
+    profilePic?: string;
+  };
+  file?: {
+    path: string;
+    filename?: string;
+    mimetype?: string;
+    size?: number;
+  };
 }
 
