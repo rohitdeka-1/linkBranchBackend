fea1d96cd24e48b2f68ab50902e02c2430d71f2e
Rohit Deka <studyrhd@gmail.com>
2025-04-18 23:27:58 +0530

    true-secured

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -70,7 +70,7 @@ const userRegistration = async (req: Request, res: Response): Promise<any> => {
 
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: isProd ? "none" : "lax",
       maxAge: 60 * 60 * 1000,
     });
