07c3aec2be994cc627a0354c1990d9018f91becf
Rohit Deka <studyrhd@gmail.com>
2025-04-19 18:13:27 +0530

    put addlinks

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -10,7 +10,7 @@ const uploadMulter = upload.single('profilePic') as unknown as express.RequestHa
 userRoute.post("/upload-image",verifyToken,uploadMulter,handleUploadImage)
 userRoute.get("/me",verifyToken,fetchUser)
 userRoute.put("/user",verifyToken,uploadMulter,updateUser)
-userRoute.post("/links",verifyToken,addLinks)
+userRoute.put("/links",verifyToken,addLinks)
 userRoute.delete("/links",verifyToken,deleteLinks)
 
 
