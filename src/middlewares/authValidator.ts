7edc505972c39574faab0081944b3fba81853cad
Rohit Deka <studyrhd@gmail.com>
2025-04-19 16:54:00 +0530

    Bug-fixes

--- src/middlewares/authValidator.ts
+++ src/middlewares/authValidator.ts
@@ -1,18 +1,66 @@
 import {Request,Response, NextFunction } from "express"
 import { body, checkSchema, validationResult } from "express-validator"
+import { register } from "module"
 
 
-export const registerationInputValidator = [
-    body('username','Username too lengthy').isLength({max:25}),
-    body('email','Invalid Email').notEmpty(),
-    body('email','Invalid Email').isEmail(),
-    body('password','Password should be greater than 3').isLength({min:4}),
-]
+// export const registerationInputValidator = [
+//     body('username','Username too lengthy').isString().isLength({max:25}),
+//     body('email','Invalid Email').notEmpty(),
+//     body('email','Invalid Email').isEmail(),
+//     body('password','Password should be greater than 3').isLength({min:4}),
+// ]
 
 // export const loginInputValidator = [
 //     body('identity','Username/Email cannot be empty').notEmpty(),
 //     body('password').notEmpty().isLength({min:4}),
 // ]
+
+export const registerationInputValidator = checkSchema(
+    {
+        username: {
+            notEmpty: {
+                errorMessage:"Username is required",
+            },
+            isString:{
+                errorMessage:"Username should be strictly string",
+            },
+            isLength:{
+                options:{
+                    max:25
+                },
+                errorMessage:"Username too lengthy"
+            },
+            trim:true,
+        },
+        email: {
+            notEmpty: {
+                errorMessage:"Email is required",
+            },
+            isEmail:{
+                errorMessage:"Invalid Email",
+            }
+        },
+        password:{
+            notEmpty:{
+                errorMessage:"Password is required"
+            },
+            isLength:{
+                options:{
+                    min:4
+                },
+                errorMessage:"Password too short"
+            }
+        },
+        fullname:{
+            notEmpty:{
+                errorMessage:"Fullname is required"
+            },
+            isString:{
+                errorMessage:"Fullname should be strictly string",
+            }
+        }
+    }
+)
 
 
 export const loginInputValidator = checkSchema(
--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -4,6 +4,15 @@ import multer from "multer";
 
 const tempDir = path.join(__dirname, "..", "public", "temp");
 
+const fileFilter = (req:Express.Request,file:Express.Multer.File,cb:Function) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only JPEG and PNG are allowed."), false);
+  }
+};
+
 if (!fs.existsSync(tempDir)) {
   fs.mkdirSync(tempDir, { recursive: true });
 }
@@ -18,6 +27,6 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage,fileFilter });
 
 export { upload };
